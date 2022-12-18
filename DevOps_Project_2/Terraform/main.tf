7271a2778bb313e2b2a1210b8db68c9c7d3426cd
Ali-iffat <120565398+Ali-iffat@users.noreply.github.com>
2022-12-19 01:09:29 +0530

    Update main.tf

--- DevOps_Project_2/Terraform/main.tf
+++ DevOps_Project_2/Terraform/main.tf
@@ -1,9 +1,10 @@
-data "aws_ami" "amazon-linux" {
-  most_recent = true
+data "aws_ami_ids" "ubuntu" {
+  owners = ["477287675722"]
+
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-2022120"]
   }
 
   filter {
@@ -13,7 +14,7 @@ data "aws_ami" "amazon-linux" {
 }
 
 resource "aws_instance" "dev_machine" {
-  ami = data.aws_ami.amazon-linux.id
+  ami = "ami-0574da719dca65348"
   instance_type = "t2.micro"
   key_name = "euran-jenkins"
 
